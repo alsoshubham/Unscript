4f11d775d9a065b6f56197b8a41b8dc183bbf7b1
alsoshubham <shubhamsahu100100@hotmail.com>
2024-10-10 02:29:17 +0530

    created components and logics

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './index.css'
+import Header from './components/Header/Header'
+import Home from './components/Home'
+import Footer from './components/Footer/Footer'
+import About from './components/About/About'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Home/>
+      <About/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+export default root;
