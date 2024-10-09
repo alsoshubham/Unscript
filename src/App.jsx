b4016e4e650d63a5c52ce36fe7a7de209ae0bc4f
alsoshubham <shubhamsahu100100@hotmail.com>
2024-10-10 03:20:32 +0530

    CSS gto scrapped

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './index.css'
 import Header from './components/Header/Header'
-import Home from './components/Home'
+// import Home from './components/Home'
 import Footer from './components/Footer/Footer'
-import About from './components/About/About'
+// import About from './components/About/About'
+// import Contact from './components/Contactpage/Contactus'
+import Layout from './Layout'
 
 function App() {
   return (
     <div>
       <Header/>
-      <Home/>
-      <About/>
+      <Layout/>
       <Footer/>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,33 @@
+// /* eslint-disable react/jsx-no-undef */
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Home from "./components/Home.jsx";
+import About from "./components/About/About.jsx";
 
-const root = createRoot(document.getElementById("root"));
+const router =  createBrowserRouter([
+    {
+        path: '/',
+        element:<Layout/>,
+        children:[
+            {
+                path:" ",
+                element:<Home/>
+            },
+            {
+                path:"about",
+                element:<About/>
+            }
+        ]
+    }
+])
 
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router = {router}/>
+  </React.StrictMode>
 );
 
-export default root;
+export default createRoot;
